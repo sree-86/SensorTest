02b7bc78c8eb69dcc376440675da6d28e3cc4337
sree-86 <sriram3rhyme@gmail.com>
2019-04-04 19:57:43 -0400

    Initial Commit

---
+++ SensorTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SensorTest
+//
+//  Created by Sreeram Ramakrishnan on 2019-04-04.
+//  Copyright © 2019 Centennial College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
